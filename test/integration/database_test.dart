e99fc70b342f33984113b9736e069306050f3f72
emacsah <emacsah@gmail.com>
2025-05-15 23:41:00 +0000

    mise a jour des scripts model

--- test/integration/database_test.dart
+++ test/integration/database_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:taxasge/database/database_service.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'package:sqflite/sqflite.dart';
 import 'dart:io';
 import 'dart:convert'; // Ajoutez cet import pour utiliser jsonDecode
 
