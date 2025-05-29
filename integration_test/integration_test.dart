0453651ad0a195c02f9f934de8852e059445e715
emacsah <emacsah@gmail.com>
2025-05-29 19:42:42 +0000

    mise a jour des dependances

--- integration_test/integration_test.dart
+++ integration_test/integration_test.dart
@@ -2,7 +2,7 @@ import 'package:integration_test/integration_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 import 'package:taxasge/main.dart' as app;
-import 'package:taxasge/test/test_utils/test_config.dart';
+import '../test/test_utils/test_config.dart';
 
 void main() {
   // Initialise l'environnement d'intégration
